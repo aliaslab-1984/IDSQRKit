a81effedf6c7d136adea22b3f3d8bc04f22fe9e4
Francesco Bianco <53116975+icesco@users.noreply.github.com>
2022-01-31 16:11:41 +0100

    Added missing depednency

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "ALConstraintKit", url: "https://github.com/aliaslab-1984/ALConstraintKit", .upToNextMajor(from: .init(1, 0, 0)))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
