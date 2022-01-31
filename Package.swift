41512acd726eae629499861521cc47501e92525e
Francesco Bianco <53116975+icesco@users.noreply.github.com>
2022-01-31 16:15:30 +0100

    Fixed missing dependency on target

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "IDSQRKit",
-            dependencies: []),
+            dependencies: ["ALConstraintKit"]),
         .testTarget(
             name: "IDSQRKitTests",
             dependencies: ["IDSQRKit"]),
